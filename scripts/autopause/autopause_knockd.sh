6c50064490bba3f5960a2c74936c8350ed3fcfbb
MusclePr <muscle@a-b-c-d.com>
2026-06-14 06:54:50 +0900

    add IP address to message of unpause reason.

--- scripts/autopause/autopause_knockd.sh
+++ scripts/autopause/autopause_knockd.sh
@@ -65,7 +65,7 @@ cat > "$KNOCKD_CONF" <<EOF
 [autopause_server_${SERVER_PORT}]
     sequence = ${SERVER_PORT}:udp
     seq_cooldown = 5
-    command = manager unpause --apply "knockd connection"
+    command = manager unpause --apply "knockd connection from %IP%"
 
 EOF
 
